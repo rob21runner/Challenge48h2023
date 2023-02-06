a6d3579f6120a3dfb0cecdbd07cb52c213e9ba88
Mohamed Diakha Ndoye <ndoyemohamed.d@gmail.com>
2023-02-06 13:15:28 +0100

    404 page style update

--- 404.php
+++ 404.php
@@ -7,10 +7,11 @@
     <title>Ynov - Hack me</title>
     <link href="https://api.fontshare.com/v2/css?f[]=satoshi@1&display=swap" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="./Content/css/style.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.4/gsap.min.js"></script>
 </head>
 
-<body>
-<main id="not-found">
+<body id="not-found">
+<main>
     <div class="error-code">404</div>
     <p>Nous sommes désolés, mais la page que vous essayez de chercher n’existe pas.</p>
 
@@ -41,6 +42,11 @@
     fox
     -->
 </main>
+
+<video src="Content/media/404-loop.webm" autoplay muted loop></video>
 </body>
 
+<script>
+    document.querySelector('video').playbackRate = 0.5;
+</script>
 </html>
