aea76f47fd2169cf231e79c3fe9dd61ad95ea1f6
Mohamed Diakha Ndoye <ndoyemohamed.d@gmail.com>
2023-02-07 10:12:36 +0100

    popup animation speed tweak

--- Content/js/popup.js
+++ Content/js/popup.js
@@ -8,16 +8,26 @@ if (modalTimeline) {
     })
     modalTimeline.from('.modal', {
         opacity: 0,
-        duration: 1.2,
+        duration: 0.4,
         ease: 'Power4.easeOut',
         y: 72
-    })
+    }, '<')
 }
 
 let closeModalButton = document.querySelector(".button[closeModal]");
 
 closeModalButton.addEventListener("click", (evt) => {
-    modalTimeline.reverse().then(() => {
+    modalTimeline.to('.modal', {
+        opacity: 0,
+        duration: 0.4,
+        ease: 'Power4.easeIn',
+        y: 72
+    })
+    modalTimeline.to('.backdrop', {
+        opacity: 0,
+        duration: 0.4,
+        ease: 'Power4.easeIn',
+    }, '<').then(() => {
         document.querySelector('.popup').style.display = 'none';
     });
 });
