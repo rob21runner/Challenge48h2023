b04b64ccf9f6051b0f58d8abce0cdcdf7f6ae296
rob21runner <robin.guerard@ynov.com>
2023-02-07 12:18:30 +0100

    bot + readme

--- README.md
+++ README.md
@@ -1,6 +1,107 @@
-# Challenge48h2023
+# Fiche réponse Lambda Corporation
 
-- [ ] Page de login
-- [ ] (Compte à rebours)
-- [ ] Challenge css
-- [ ] CHallenge js
+## Page d'accueil
+
+En premier lieu nous arrivons sur la page d'accueil donnant un contexte au challenge.
+
+Rien de spécial sur cette page juste un bouton pour nous amener à la page suivante.
+
+## Erreur 404
+
+Cette page 404 est un piège il y a un rick roll en arrière plan pour induire en erreur et faire penser qu'il fallait cliquer autre part.
+
+Mais cette page est bien la suite, si nous allons dans le code source nous pouvons voir en commentaire des caractères spéciaux :
+
+```
+    Lorem
+    amet
+    sit
+    consectetur
+    ipsum
+    dolor
+    vrai
+    plait
+    login
+    plait
+    Le
+    vous
+    s'il
+    dog
+    brown
+    lazy
+    the
+    quick
+    the
+    jumps
+    fox
+    over
+```
+
+Ce qui nous permet d'obtenir 3 phrases :
+
+* Lorem ipsum dolor sit amet consectetur
+
+* Le vrai login s'il vous plait
+
+* The quick brown fox jumps over the lazy dog
+
+Au bout de 3 minutes nous avons un premier indice qui est : `Les bons mots, le bon ordre et on navigue. Avec le .php pour finir bien-sûr`
+
+Nous voulons prouver ici deux choses: 
+
+* Laisser un commentaire comportant des informations confidentiels peut être dangereux et laisser un indice pouvant mener à une faille.
+
+* Un url non protégé peut mener à une vulnérabilité informatique qui peut se produire lorsqu'une application Web utilise des entrées utilisateur dans une URL sans les valider ou les filtrer adéquatement.
+
+La réponse à cette étape était de mettre `Lorem-ipsum-dolor-sit-amet-consectetur.php` dans l'url.
+
+## login-page.php
+
+Sur cette page l'utilisateur se trouve face à un login.
+
+La méthode pour passer cette étape est de faire un injection sql : `' or 1 =1 ;`
+
+Il y a un petit piège, le bouton de connexion est un faux, il évite votre souris, il y a un réel bouton de connexion transparent qui peut être cliquer aléatoirement ou trouver en inspectant la page.
+
+## user-page.php
+
+Cette page présente l'injection et comment l'éviter.
+Dans l'url il suffit de remplacer user par admin dans l'url pour se rendre sur `admin-page.php`
+
+## admin-page.php
+
+Cette page contient un code binaire : `01110101 01110010 01101100 00111101 01110100 01100101 01110011 01110100 00101110 01110000 01100100 01100110`
+
+Il suffit de le convertir en texte et ça nous donne : `url=test.pdf`
+
+si la personne met dans l'url `test.pdf` ça lui affiche un pdf.
+
+## Le PDF
+
+Le pdf est chiffré en César-10. Si l'utilisateur traduit les différents textes du pdf il trouve l'url d'un serveur discord `dtUveqypNA` et un salon `analyse-lambda-bot` et un utilisateur `XxDylanKillerxX`.
+
+## Le serveur Discord
+
+Le serveur Discord contient un bot qui met une liste de failles, dans le salon `analyse-lambda-bot` pour des ip de clients du serveur avec un mot de passe.
+
+Là il faut retrouver l'utilisateur donné, prendre le mot de passe et se rendre sur l'ip donnée qui est `10.13.33.222:1194/login-page-2.php`
+
+## Le site de XxDylanKillerxX
+
+Sur ce site on retrouve un formulaire à remplir, il suffit de mettre l'identifiant `XxDylanKillerxX` et le mot de passe `Je suis dylan le Kikoo`
+
+On est redirigé sur une page avec un lien qui contient un lien confidentiel (win-page.php).
+
+## win-page.php
+
+On retrouve le lien d'un doc confidentiel dans lequel on félicite le joueur et on lui donne la commande à effectuer sur discord `λhacker`. Un rôle lui sera donné afin qu'il puisse accéder aux explications.
+
+## Easter-egg
+
+![](C:\Users\kioki\AppData\Roaming\marktext\images\984c34d8f2250f48f465b0856ce3ca0384c1989f.png)
+
+Ici nous avons Lambda and co
+
+La dernière commande `λhacker` cela sous entends que pour effectuer une commande avec le bot il faut utiliser λ.
+
+Donc la commande est `λ&co`
---
+++ bot/index.js
@@ -0,0 +1,114 @@
+const Discord = require('discord.js'),
+    client = new Discord.Client({
+        fetchAllMembers: true,
+        partials: ["MESSAGE","REACTION"]
+    })
+client.on('ready', () => {
+    console.log('Je suis en ligne!')
+    createJson()
+    createTemplate()
+})
+
+client.on('error', () => {
+    console.log('Je suis en ligne!')
+    //LogGen("Erreur","Une Erreur est survenue !","#ff3838")
+})
+
+client.login("MTA3MjE1MzU4NjU4ODQ1MDgzNw.GhsoxX.eKU3EBtlGLT5ZDsrRSzJzA44XiUaHdC0aUiJBc")
+client.commands = new Discord.Collection()
+
+premier = true
+
+client.on('message', message => {
+    if (message.type !== 'DEFAULT' || message.author.bot) return
+    if (message.content.startsWith("λ&co")) {
+        if (premier) {
+            premier = false
+            message.channel.send(new Discord.MessageEmbed()
+                .setTitle("Bravo")
+                .setAuthor("Lambda Bot","https://cdn.discordapp.com/emojis/809341286800162837.png")
+                .setDescription("Bravo <@"+ message.author.id +"> tu as gagné un paquet de Dragibus!")
+                .setFooter("Dev by Lambda Corporation")
+                .setColor('#9b59b6'))
+        }  
+    }
+    if (message.content.startsWith("λhacker")) {
+        const member = message.guild.member(user)
+        member.roles.add("1072465183420710932")
+        client.channels.cache.get("1072468802903736340").send("@<1072080475914502144> @<"+ message.author.id +"> a finit!")
+    }
+})
+
+var analysis = [];
+function createJson() {
+    names = require('./names.json')
+    j = 0
+    for (var i = 0; i < 527; i++) {
+        var id = j;
+        var generatedIp = "192.168." + Math.floor(Math.random() * 256) + "." + Math.floor(Math.random() * 256);
+        var customUser = names.firstname[i] + " " + names.lastname[i];
+        var customPassword = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+        var breaches = ["SQL injection", "Cross-Site Scripting", "Remote Code Execution", "Denial of Service"];
+        var randomBreach = breaches[Math.floor(Math.random() * breaches.length)];
+        var details = "The " + randomBreach + " vulnerability has been discovered in the system. It is recommended to patch the system and implement proper security measures to prevent similar incidents in the future.";
+        analysis.push({
+            id: id,
+            ip: generatedIp,
+            user: customUser,
+            password: customPassword,
+            breach: randomBreach,
+            details: details
+        });
+        j++
+        if (i == 30) {
+            id = j
+            j++
+            generatedIp = "http://10.13.33.222:1194/login-page-2.php"
+            customUser = "XxDylanKillerxX"
+            customPassword = "Je suis dylan le Kikoo"
+            randomBreach = "Everything is open!!!"
+            details = "The " + randomBreach + " vulnerability has been discovered in the system. It is recommended to patch the system and implement proper security measures to prevent similar incidents in the future."
+            analysis.push({
+                id: id,
+                ip: generatedIp,
+                user: customUser,
+                password: customPassword,
+                breach: randomBreach,
+                details: details
+            });
+        }
+    }
+
+    //console.log(JSON.stringify(analysis));
+}
+  
+
+function createTemplate() {
+    analysis.forEach(function(i) {
+        id = i.id
+        ip = i.ip
+        user = i.user
+        password = i.password
+        breach = i.breach
+        details = i.details
+
+        title = "Web site analysis #"+i.id
+        text = "    - ip : "+ i.ip +"\n    - user : "+ i.user +"\n    - password : "+ i.password +"\n    - breach : "+i.breach +"\n"+details
+            
+        LogGen(title, text, '#0984e3') 
+    })
+}
+
+function LogGen(title, message, color) {
+    if (color == null) {
+        color = "#d3d3d3"
+    }
+    console.log(message)
+    client.channels.cache.get("1072466827663384606").send(new Discord.MessageEmbed()
+        .setTitle(title)
+        .setAuthor("Lambda Bot","https://cdn.discordapp.com/emojis/809341286149783563.png")
+        .setDescription(message)
+        .setFooter("Dev by Lambda Corporation")
+        .setColor(color)
+    )
+}
